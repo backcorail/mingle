045d9d314b81c3ef16140e7e77d9734c179e41e6
User <User@DESKTOP-SISV6CC>
2023-11-24 14:34:23 +0900

    home _front, resell_front, Kream crawlimg

---
+++ src/main/java/com/project/mingle/vo/KreamVO.java
@@ -0,0 +1,14 @@
+/*
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+public class KreamVO{
+    private String image;
+    private String subject;
+    private String url;
+}
+*/
