91554b827bb79733b45c6480bac105d0906cc5dd
USER <USER@DESKTOP-GC0B4I9>
2023-12-05 19:50:31 +0900

    슬라이드 리스트 완성

--- src/main/resources/static/js/cody_board.js
+++ src/main/resources/static/js/cody_board.js
@@ -1,4 +1,86 @@
+var $item = 170;
+var $count = $('.slide_item1').length;
+var $count2 = $('.slide_item2').length;
+var $count3 = $('.slide_item3').length;
+var $count4 = $('.slide_item4').length;
+
+var $slidebox = $item*$count+50;
+var countS = 0;
+var count2 = 0;
+var count3 = 0;
+var count4 = 0;
+
+function moveSlider(){
+	var check = $(this).attr('data-btn');
+	if(check==0&& countS>0){
+		countS -= 1;
+		$('.slide_list').animate({left : '+='+$item+'px'},300,slideEnd)
+	}else if(check == 1&&countS<($count-4)){
+		countS += 1;
+		$('.slide_list').animate({left : '-='+$item+'px'},300,slideEnd)
+	}
+}
+
+function moveSlider2(){
+	var check = $(this).attr('data-btn');
+	if(check==0&& count2>0){
+		count2 -= 1;
+		$('.slide_list2').animate({left : '+='+$item+'px'},300,slideEnd)
+	}else if(check == 1&&count2<($count2-5)){
+		count2 += 1;
+		$('.slide_list2').animate({left : '-='+$item+'px'},300,slideEnd)
+	}
+}
+function moveSlider3(){
+	var check = $(this).attr('data-btn');
+	if(check==0&& count3>0){
+		count3 -= 1;
+		$('.slide_list3').animate({left : '+='+$item+'px'},300,slideEnd)
+	}else if(check == 1&&count3<($count3-5)){
+		count3 += 1;
+		$('.slide_list3').animate({left : '-='+$item+'px'},300,slideEnd)
+	}
+}
+function moveSlider4(){
+	var check = $(this).attr('data-btn');
+	if(check==0&& count4>0){
+		count4 -= 1;
+		$('.slide_list4').animate({left : '+='+$item+'px'},300,slideEnd)
+	}else if(check == 1&&count4<($count4-5)){
+		count4 += 1;
+		$('.slide_list4').animate({left : '-='+$item+'px'},300,slideEnd)
+	}
+}
+
+
+function slideEnd(){
+	var nowLeft = $('.slide_list').position().right;
+	var end = -($slidebox-710);
+	
+	if(nowLeft<=end){
+		$('.slide_list').animate({right : end});
+	}else if(nowLeft>0){
+		$('.slide_list').animate({right : 0});
+	}
+}
+
+
+
 $(document).ready(function(){
+		$('.slide_list').css('width',$slidebox);
+		$('.slide_list2').css('width',$slidebox);
+		$('.slide_list3').css('width',$slidebox);
+		$('.slide_list4').css('width',$slidebox);
+		
+		$('.leftBtn').on('click',moveSlider);
+		$('.rightBtn').on('click',moveSlider);
+		$('.leftBtn2').on('click',moveSlider2);
+		$('.rightBtn2').on('click',moveSlider2);
+		$('.leftBtn3').on('click',moveSlider3);
+		$('.rightBtn3').on('click',moveSlider3);
+		$('.leftBtn4').on('click',moveSlider4);
+		$('.rightBtn4').on('click',moveSlider4);
+		
 	    var day_names= ["일","월","화","수","목","금","토"]
 	    var date = new Date();
 	    $("#dateName").text(day_names[date.getDay()]);
