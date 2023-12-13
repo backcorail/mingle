638af1fe64ef0602e9c89c29261c17fe0cb8bfe5
soon1541 <rkqkrdlans123@naver.com>
2023-12-13 16:31:41 +0900

    12/13 오류 수정

--- src/main/resources/static/js/style_styles.js
+++ src/main/resources/static/js/style_styles.js
@@ -13,5 +13,20 @@ $(document).on('click', '.likeLink1, .likeLink2', function(e) {
     e.preventDefault();
     console.log('asd');
     
-   
+      var $this = $(this);
+        var $img = $this.find('img.like1');
+        var $span = $this.find('span');
+        var count = parseInt($span.text(), 10);
+
+        if ($img.attr('src') === '/mingle/img/img_mingle/page_recell/icon_heart.png') {
+            // 하트가 비워져 있을 경우
+            $img.attr('src', '/mingle/img/style/찬하트 사진.jpg'); // 하트 채우기
+            $span.text(count + 1); // span 값 1 증가
+        } else {
+            // 하트가 채워져 있을 경우
+            $img.attr('src', '/mingle/img/img_mingle/page_recell/icon_heart.png'); // 하트 비우기
+            $span.text(count - 1); // span 값 1 감소
+        }
     });
+   
+
