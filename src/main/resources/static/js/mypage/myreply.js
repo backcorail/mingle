53eb21d56bf1110b511cb80c0049fa96a5efab1b
backcorail <backpro1024@gmail.com>
2023-12-15 07:52:46 +0900

    마이페이지 댓글 내용보기 추가수정

--- src/main/resources/static/js/mypage/myreply.js
+++ src/main/resources/static/js/mypage/myreply.js
@@ -30,25 +30,27 @@ $(function() {
 				// map을 사용하여 각 actInfo를 HTML 태그로 변환
 				var newHtml = response.actInfos.map(function(actInfo) {
 					return `
-                <div class="div-track">
-                    <div class="div-board-cate-area">
-                        <div class="div-board-cate Inika-bold-18">${actInfo.type}</div>
-                    </div>
-                    <div class="div-board-title-area">
-                        <a href="http://localhost:9998/mingle/resell/board?no=${actInfo.no}">
-                        <div class="div-board-title Inika-Regular-16-l2">${actInfo.title}</div>
-                        </a>
-                    </div>
-                    <div class="div-board-date-area">
-                        <div class="div-board-date Inika-Regular-20">${actInfo.writeDate}</div>
-                    </div>
-                </div>`;
+					<div class="div-track">
+					<div class="div-board-cate-area">
+						<div class="div-board-cate Inika-bold-18">${actInfo.type}</div>
+					</div>
+					<div class="div-board-title-area">
+						<a href="http://localhost:9998/mingle/resell/board?no=${actInfo.no}">
+							<div class="div-board-title Inika-Regular-16-l2">${actInfo.title}</div>
+						</a>						
+					</div>
+					<div class="div-reply-content-area">
+						<div class="div-reply-content Inika-Regular-20">${actInfo.contents}</div>
+					</div>
+					<div class="div-board-date-area">
+						<div class="div-board-date Inika-Regular-20">${actInfo.writeDate}</div>
+					</div>
+				</div>`;
 				}).join('');
 
 				// hidden input 필드의 값을 업데이트
 				$("#requestno").val(response.lastmap['리퀘스트']);
 				$("#styleno").val(response.lastmap['스타일']);
-				$("#resellno").val(response.lastmap['리셀']);
 
 				// 생성된 HTML 태그를 페이지에 추가
 				var itemListArea = $(".div-item-list-area");
