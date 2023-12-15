2eb73af950c0d1ceb01014bca12d54568c4060a4
User <User@DESKTOP-SISV6CC>
2023-12-15 14:54:32 +0900

    home, header, style  수정

--- src/main/java/com/project/mingle/controller/HomeController.java
+++ src/main/java/com/project/mingle/controller/HomeController.java
@@ -9,20 +9,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.project.mingle.service.HomeService;
+import com.project.mingle.service.StyleService;
 import com.project.mingle.vo.ResellVO;
+import com.project.mingle.vo.StyleVO;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 	@Autowired
 	HomeService service;
-	
+	@Autowired
+	StyleService service2;
 	
 	@GetMapping("")
-	public String home(Model model, ResellVO rvo) {
+	public String home(Model model, ResellVO rvo, StyleVO sVO) {
 		List<ResellVO> kreamList = service.kreamList(rvo);
+		List<StyleVO> kreamList2 = service2.kreamData(sVO);
 		model.addAttribute("rvo", rvo);
+		model.addAttribute("sVO", sVO);
 		model.addAttribute("kreamList", kreamList.subList(1, 6));
+		model.addAttribute("kreamList2", kreamList2.subList(1, 7));
     
 		return "home";
 	}
